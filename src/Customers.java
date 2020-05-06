03f4711a3abb3adbd5f5ac639594e3b38963abae
TheOdie <63706431+TheOdie@users.noreply.github.com>
2020-05-06 23:32:40 +0300

    Add files via upload

---
+++ src/Customers.java
@@ -0,0 +1,16 @@
+public class Customers {
+
+    private Orders[] orders;
+
+    private String Name;
+
+    private int NumberCustomer;
+
+    public Orders addOrder(Orders orders) {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    public Orders canOrder(Orders orders) {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+}
---
+++ src/Orders.java
@@ -0,0 +1,20 @@
+public class Orders {
+
+    private Foods[] foods;
+
+    private Customers orderer;
+
+    private int NumberOrder;
+
+    public float getOrderPrice() {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    public Foods addFood(Foods foods) {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    public Foods delFood(Foods foods) {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+}
